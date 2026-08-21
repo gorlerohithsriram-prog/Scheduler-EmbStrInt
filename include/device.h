d1e0ce73ce5649c041e48c65456e6db48706423c
Rohith Gorle <gorlerohithsriram@gmail.com>
2026-08-21 22:19:20 +0530

    Add device.h header file with device structures

---
+++ include/device.h
@@ -0,0 +1,45 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+typedef enum {
+    DEVICE_MAX78000,
+    DEVICE_ESP32C6
+} DeviceType;
+
+typedef enum {
+    DEVICE_IDLE,
+    DEVICE_BUSY,
+    DEVICE_OFFLINE
+} DeviceStatus;
+
+typedef struct {
+    int adc;
+    int pwm;
+    int uart;
+    int wifi;
+    int ble;
+    int cnn;
+} DeviceCapabilities;
+
+typedef struct {
+    int ram_total;
+    int ram_free;
+
+    int cpu_usage;
+
+    int queue_length;
+    int queue_capacity;
+} DeviceResources;
+
+typedef struct {
+    int id;
+    DeviceType type;
+
+    DeviceStatus status;
+
+    DeviceCapabilities capabilities;
+    DeviceResources resources;
+
+} Device;
+
+#endif
