321d20f4fdb686045ce7aac72bf728f153bdaaa4
Rohith Gorle <gorlerohithsriram@gmail.com>
2026-09-02 21:37:00 +0530

    Add SchedulerWeights struct and update schedule_task

--- include/scheduler.h
+++ include/scheduler.h
@@ -4,15 +4,28 @@
 #include "task.h"
 #include "device.h"
 
+typedef struct {
+    double cpu_weight;
+    double queue_weight;
+    double latency_weight;
+} SchedulerWeights;
+
 int device_can_run_task(
     const Task *task,
     const Device *device
 );
 
+double calculate_device_score(
+    const Task *task,
+    const Device *device,
+    const SchedulerWeights *weights
+);
+
 Device *schedule_task(
     Task *task,
     Device devices[],
-    int device_count
+    int device_count,
+    const SchedulerWeights *weights
 );
 
 #endif
